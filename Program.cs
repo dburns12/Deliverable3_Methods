2e662bb6f8ef79e9c9e05729ce02dcf7854d55d5
dburns12 <dburns1@mail.usf.edu>
2019-09-18 17:47:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Author: Derrick Burns
+    Date: 9/18/2019
+    Comments: This application code demonstrates the use of methods.
+ */
+
+using System;
+
+namespace Deliverable3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your first and last name: ");
+            try
+            {
+                // This variable will gather data from user input
+                string input = Console.ReadLine();
+
+                Console.WriteLine("\n");
+
+                // Call Print_Name method
+                Print_Name(input);
+            }
+            catch
+            {
+
+            }
+        } // End of main
+        
+        // Method that displays user input
+        private static void Print_Name(string input)
+        {
+            Console.WriteLine("Hello " + input + "!");
+        } // End of Print_Name
+    } // End of class
+} // End of namespace
